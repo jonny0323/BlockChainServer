474b9fcde4950701ec1fdf839337961de2ffafdc
TaeJoon Lee <taejoonlee@TaeJoonui-noteubug.local>
2025-11-24 12:14:23 +0900

    commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,15 @@
 import mysql from 'mysql2';
 
-
-// 💡 중요: 실제 비밀번호와 호스트는 환경 변수(.env)에 저장하여 사용해야 합니다.
+// 환경변수 사용
 export const pool = mysql.createPool({
-    host: 'localhost',         
-    user: 'jonny',             // ✨ jonny로 변경
-    password: '0323',          // ✨ 비밀번호 '0323'로 변경
-    database: 'BlockChain', 
-    waitForConnections: true,  
-    connectionLimit: 10,       
-    queueLimit: 0              
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 }).promise();
 
 console.log("MySQL Connection Pool Created.");
